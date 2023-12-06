a1008f14c9957ddc58349a8d5c42ee9f9038c2ee
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-07 01:39:02 +0530

    Create App.jsx

---
+++ ReactLearning/src/App.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./context/auth-context";
+
+function App() {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <>
+      <MainHeader />
+      <main>{ctx.isLoggedIn ? <Home /> : <Login />}</main>
+    </>
+  );
+}
+
+export default App;
